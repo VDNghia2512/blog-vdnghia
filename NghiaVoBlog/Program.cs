42977c99608a711b0649e5ac730ca79d5385015a
vdnghia2512 <vdnghia2512@gmail.com>
2022-09-30 16:47:00 +0700

    UserController

--- NghiaVoBlog/Program.cs
+++ NghiaVoBlog/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using NghiaVoBlog.Data;
+using NghiaVoBlog.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,8 @@ builder.Services.AddDbContext <AppDBContext>(option =>
 {
     option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddScoped<UserRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
